e536441dcdfb402f0f5727233549e34d97664fd6
Pablo Yuri <pabloyuridev@gmail.com>
2024-06-27 02:47:28 -0300

    feat: add the algorithm to insert the timestamp

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,16 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'timestamp-inserter.insert',
-    () => {}
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const selection = editor.selection;
+        const timestamp = Date.now().toString();
+        editor.edit((editBuilder) => {
+          editBuilder.replace(selection, timestamp);
+        });
+      }
+    }
   );
 
   context.subscriptions.push(disposable);
